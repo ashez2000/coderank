ab3eebefb7287402bf409c56e56c333160fb1f8b
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-07-14 20:33:11 +0530

    eval: Add nodemon

--- eval/src/app.js
+++ eval/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import { evalSnippet } from './eval.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
